de846b56947f891452c6658216807097854aa794
Jamie Mcstay <jamiemcstay@hotmail.com>
2024-07-17 12:30:52 +0000

    Add main function and ask task to update tasks list

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 
+
 tasks = []
 
 def show_options():
@@ -8,7 +9,20 @@ def show_options():
     print("2.View tasks")
     print("3.Remove tasks")
 
-def add_tasks()
+def add_task(task):
+    tasks.append(task)
+    print(f"Task Added: {task}")
+    print(f"your tasks are: {tasks}")
 
-show_options()
-add_tasks()
+
+def main():
+    while True:
+        show_options()
+        choose_option = input("Choose an option: ")
+        if choose_option == '1':
+            task = input("Enter your task: ")
+            add_task(task)
+        
+    
+
+main()
