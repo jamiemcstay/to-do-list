4e718dac4895ae16014dcdc6a374dfd0d855ec38
Jamie Mcstay <jamiemcstay@hotmail.com>
2024-07-17 10:43:29 +0000

    Add function to show options for user

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import json
+
+tasks = []
+
+def show_options():
+    print("ToDo List")
+    print("1.Add a task")
+    print("2.View tasks")
+    print("3.Remove tasks")
+
+def add_tasks()
+
+show_options()
+add_tasks()
